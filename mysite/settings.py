c352785e23dbafe1eca8059828c84e3ee4b2acd7
your lamia <lamiakessab4@gmail.com>
2025-08-21 23:05:41 +1200

    fix deployment files

--- mysite/settings.py
+++ mysite/settings.py
@@ -121,7 +121,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR, 'mysite/static')
 ]
-STATIC_ROOT= os.path.join(BASE_DIR, 'static')
+STATIC_ROOT= os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE="whitenoise.storge.CompressedManifestStaticFilesStorage"
 #MEDIA
 MEDIA_ROOT= os.path.join(BASE_DIR, 'media')
